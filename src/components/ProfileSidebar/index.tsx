3002281255b962b7b1877b69121cf3ba483a1331
Lindsay Ferreira <eson@iouu.com.br>
2020-11-17 16:28:40 -0300

    feat: criar componente ProfileSidebar

---
+++ src/components/ProfileSidebar/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProfileInfo from "../ProfileInfo";
+
+import "./styles.css";
+
+const ProfileSidebar = () => {
+  return (
+    <div className="profile">
+      <ProfileInfo />
+    </div>
+  );
+};
+
+export default ProfileSidebar;

955b4dc468580441ae53cd8b272624e39c88bced
Lindsay Ferreira <eson@iouu.com.br>
2020-11-17 16:48:40 -0300

    feat: criar pagina de Login

---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LoginBox from "../../components/LoginBox";
+
+import "./styles.css";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <LoginBox />
+    </div>
+  );
+};
+
+export default Login;
